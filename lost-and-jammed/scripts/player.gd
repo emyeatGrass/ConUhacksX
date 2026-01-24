d6e804816ee78c5563e0b086506ce288f6b069d0
Kian Chowanietz <kianlettres@gmail.com>
2026-01-24 18:31:01 -0500

    Fix blurry bug

--- lost-and-jammed/scripts/player.gd
+++ lost-and-jammed/scripts/player.gd
@@ -1,12 +1,32 @@
 extends CharacterBody2D
 
+@onready var _animated_sprite = $AnimatedSprite2D
 
 const SPEED = 300.0
+var last_direction;
+const direction_to_idle = {
+	"right": "idle_side",
+	"left": "idle_side",
+	"up": "idle_up",
+	"down": "idle_down"
+}
 
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_right"):
+		last_direction = "right";
+		_animated_sprite.flip_h = false;
+	elif Input.is_action_pressed("move_left"):
+		last_direction = "left";
+		_animated_sprite.flip_h = true;
+	elif Input.is_action_pressed("move_up"):
+		last_direction = "up";
+	elif Input.is_action_pressed("move_down"):
+		last_direction = "down";
+
+	if (last_direction != null):
+		_animated_sprite.play(direction_to_idle[last_direction]);
 
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction:
 		velocity = direction * SPEED
