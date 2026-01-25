33f3a87fd9637e1671aa6a18b8ebcebe68ba9761
Maxime Huynh <70218544+maxhu2001@users.noreply.github.com>
2026-01-24 21:38:06 -0500

    removed the hearts

--- lost-and-jammed/scenes/hud.gd
+++ lost-and-jammed/scenes/hud.gd
@@ -1,12 +1,11 @@
 extends CanvasLayer
 
-@export var heart_full: Texture2D
-@export var heart_empty: Texture2D
 
-@onready var hearts_box: HBoxContainer = $Hearts
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
 
-func set_hearts(current: int, max_hearts: int) -> void:
-	# Assumes you have exactly max_hearts children (3 in your case)
-	for i in range(hearts_box.get_child_count()):
-		var heart := hearts_box.get_child(i) as TextureRect
-		heart.texture = heart_full if i < current else heart_empty
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
